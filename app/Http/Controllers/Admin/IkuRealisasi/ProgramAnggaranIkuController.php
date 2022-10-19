47ece8bf9c43d89349ab3a90ef449975b02f1f44
EkyGalih <ekkygalih8@gmail.com>
2022-10-19 21:05:36 +0800

    fix iku realisasi bagian persen

--- app/Http/Controllers/Admin/IkuRealisasi/ProgramAnggaranIkuController.php
+++ app/Http/Controllers/Admin/IkuRealisasi/ProgramAnggaranIkuController.php
@@ -40,19 +40,11 @@ class ProgramAnggaranIkuController extends Controller
 
         $getPersentase = ($request->anggaran_terpakai / $request->anggaran) * 100;
 
-        if (round($getPersentase) == 1.0 || round($getPersentase) == 0.0) {
-            $persentase = round($getPersentase * 100);
-            $ProgramAnggaran->update([
-                'anggaran_terpakai' => $request->anggaran_terpakai,
-                'persentase_anggaran' => $persentase
-            ]);
-        } elseif(round($getPersentase) != 1.0 || round($getPersentase) != 0.0) {
-            $persentase = round($getPersentase);
-            $ProgramAnggaran->update([
-                'anggaran_terpakai' => $request->anggaran_terpakai,
-                'persentase_anggaran' => $persentase
-            ]);
-        }
+        $ProgramAnggaran->update([
+            'anggaran_terpakai' => $request->anggaran_terpakai,
+            'persentase_anggaran' => $getPersentase
+        ]);
+
 
         return redirect()->route('iku-realisasi.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
