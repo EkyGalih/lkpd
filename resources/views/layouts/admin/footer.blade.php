e626311036275a1fcfe5871fe14f24ba748e0b1e
Eky Galih <ekkygalih8@gmail.com>
2022-11-18 20:56:32 +0800

    ubah footer

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,10 +1,9 @@
 <footer class="site-footer">
     <div class="text-center">
       <p>
-        &copy; Powered By <strong><a style="color: #ffffff" href="http://bpkad.ntbprov.go.id">BPKAD Prov.NTB</a></strong>. 2015 - {{ date('Y') }} All Rights Reserved
-      </p>
+        @Powered By <strong><a style="color: #ffffff" href="http://bpkad.ntbprov.go.id">BPKAD Prov.NTB</a></strong>. 2014 - {{ date('Y') }}
       <div class="credits">
-        Created @ iTeam BPKAD
+        &copy;iTeam BPKAD
       </div>
       <a href="index.html#" class="go-top">
         <i class="fa fa-angle-up"></i>
