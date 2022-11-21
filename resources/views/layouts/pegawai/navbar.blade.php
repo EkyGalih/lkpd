d40c319290cd61332b8f001e5f26e6b8e462ccae
Eky Galih <ekkygalih8@gmail.com>
2022-11-21 09:34:03 +0800

    hide navbar when mode mobile

--- resources/views/layouts/pegawai/navbar.blade.php
+++ resources/views/layouts/pegawai/navbar.blade.php
@@ -3,7 +3,7 @@
         <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
     </div>
     <!--logo start-->
-    <a href="/" class="logo"><b>APBD<span> HandBook</span></b></a>
+    <a href="{{ route('pegawai') }}" class="logo"><b>APBD<span> HandBook</span></b></a>
     <!--logo end-->
     <div class="nav notify-row" id="top_menu">
         <!--  notification start -->
